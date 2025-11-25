83315ff70190a976984c793a487a563cfaac3832
gabrielAlencar33564 <gabrielalencar33564@gmail.com>
2025-11-25 01:14:07 -0300

    feat: collector config

---
+++ collector/config/settings.py
@@ -0,0 +1,9 @@
+import os
+
+API_URL = "https://api.open-meteo.com/v1/forecast"
+
+RABBITMQ_URL = os.getenv('RABBITMQ_URL', 'amqp://guest:guest@rabbitmq:5672/')
+QUEUE_NAME = os.getenv('QUEUE_NAME', 'weather_data')
+
+CITY_LAT = os.getenv('CITY_LAT', "-12.51")
+CITY_LON = os.getenv('CITY_LON', "-41.57")
